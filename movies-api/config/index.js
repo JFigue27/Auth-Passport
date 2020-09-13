efa07d407c67ee3d5ac0c16747ffad6a8d367ee7
jjfigueroa <josejaime.figueroa@molex.com>
2020-09-13 15:56:46 -0600

    Data Seed Scripts

--- movies-api/config/index.js
+++ movies-api/config/index.js
@@ -8,7 +8,12 @@ const config = {
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiToken: process.env.PUBLIC_API_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
 };
 // console.log(`Inside Config:`, JSON.stringify(config, null, 3));
 
